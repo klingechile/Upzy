c677274f9ce0391a2ded7ffa6b57b9d86e8fc083
klingechile <klingechile@gmail.com>
2026-05-30 17:35:26 -0400

    feat(frontend): route upzy review to sprint 2

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.get('/health', (req, res) => {
 
 app.get('/', (req, res) => res.redirect('/dashboard'));
 app.get('/dashboard', (req, res) => res.sendFile('dashboard.html', { root: 'public' }));
-app.get('/upzy',      (req, res) => res.sendFile('upzy-sprint1.html', { root: 'public' }));
+app.get('/upzy',      (req, res) => res.sendFile('upzy-sprint2.html', { root: 'public' }));
 app.get('/login',     (req, res) => res.sendFile('login.html',     { root: 'public' }));
 
 
